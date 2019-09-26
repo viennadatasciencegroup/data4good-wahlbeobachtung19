f6f240a49bd6b21b7b50fa23ea55905fdd42893e
datadonK23 <datadonk23@gmail.com>
2019-09-26 18:25:06 +0200

    Ads Archive scraper. WIP, status - all from one source."

--- src/data/fetch_ad_lib.py
+++ src/data/fetch_ad_lib.py
@@ -20,24 +20,18 @@ from config import credentials
 
 
 
-def fetch_ads(id):
-    """ FIXME
+def request(url, params=None):
+    """ Make API request
 
-    :param id:
-    :return:
+    :param url: page URL
+    :param params: parameters of request
+    :return: response
     """
-    ads_archive_url = "https://graph.facebook.com/v4.0/ads_archive"
-
     try:
-        response = requests.get(ads_archive_url,
-                     params={
-                         "ad_reached_countries": "['AT']",
-                         "ad_type": "POLITICAL_AND_ISSUE_ADS",
-                         "fields": "spend,page_name",
-                         "limit": 800,
-                         "search_page_ids": id,
-                         "access_token": credentials.ad_lib_access_token
-                             })
+        if params:
+            response = requests.get(url, params)
+        else:
+            response = requests.get(url)
 
         # If the response was successful, no Exception will be raised
         response.raise_for_status()
@@ -46,10 +40,53 @@ def fetch_ads(id):
     except Exception as err:
         print(f'Other error occurred: {err}')
     else:
-        print('Success!')
-        print(response.json())
+        print("Request successful")
+        return response
 
-    return id
+
+def scrape_ads(id):
+    """ Scrape ads from one source (specified by user ID).
+
+    :param id: user ID
+    :return: list of ad dicts
+    """
+    ads_archive_url = "https://graph.facebook.com/v4.0/ads_archive"
+    fields = "ad_creation_time,ad_creative_body,ad_creative_link_caption," \
+             "ad_creative_link_description,ad_creative_link_title," \
+             "ad_delivery_start_time,ad_delivery_stop_time,ad_snapshot_url," \
+             "currency,demographic_distribution,funding_entity,impressions," \
+             "page_id,page_name,region_distribution,spend"
+    params = {"ad_active_status": "ALL",
+              "ad_reached_countries": "['AT']",
+              "ad_type": "POLITICAL_AND_ISSUE_ADS",
+              "fields": fields,
+              "limit": 25,
+              "search_page_ids": id,
+              "access_token": credentials.ad_lib_access_token}
+
+    # Inital request
+    response = request(ads_archive_url,params)
+    response_json = response.json()
+    #FIXME check if in relevant timeframe
+    results = response_json["data"]
+
+    # Pagination requests
+    last_page = False
+    while not last_page:
+        try:
+            if "next" in response_json["paging"].keys():
+                url = response_json["paging"]["next"]
+                response = request(url)
+                response_json = response.json()
+                if len(response_json["data"]) != 0:
+                    results.append(response_json["data"])
+                print(len(response_json["data"]))
+            else:
+                last_page = True
+        except KeyError:
+            last_page = True
+
+    return results
 
 
 if __name__ == "__main__":
@@ -57,6 +94,22 @@ if __name__ == "__main__":
 
     logging.info("Load Fanpages list")
     fanpages_df = pd.read_csv(os.path.join(data_path, "Test_AdsList.csv"))
+    #FIXME loop through sources
     #for _, row in fanpages_df.iterrows():
     #    ads_data = fetch_ads(row.ID)
-    fetch_ads(fanpages_df.ID.head(1).values[0])
+    results = scrape_ads(fanpages_df.ID.head(1).values[0])
+    print(len(results))
+
+    scraped_data = pd.DataFrame(results, columns=[
+        "ad_creation_time", "ad_creative_body", "ad_creative_link_caption",
+        "ad_creative_link_description", "ad_creative_link_title",
+        "ad_delivery_start_time", "ad_delivery_stop_time", "ad_snapshot_url",
+        "currency", "demographic_distribution", "funding_entity",
+        "impressions", "page_id", "page_name", "region_distribution", "spend"])
+
+    for res in results:
+        print(len(res))
+        print(res)
+
+    print(scraped_data.shape)
+    print(scraped_data.ad_delivery_stop_time)
