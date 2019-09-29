740560b7a2300ec610a8f2a96db25bb8f44f7280
datadonK23 <datadonk23@gmail.com>
2019-09-29 17:50:57 +0200

    Preprocessing script refactored. Text preprocessing enhanced. DF cleaning applied to new raw data schema.

--- src/nlp/preprocess_dataset.py
+++ src/nlp/preprocess_dataset.py
@@ -4,7 +4,7 @@
 Preprocess dataset of FB and Twitter data.
 
 Author: datadonk23
-Date: 24.09.19 
+Date: 29.09.19
 """
 
 import os, re, string
@@ -15,24 +15,6 @@ nltk.data.path.append(nltk_data_path)
 from nltk import word_tokenize
 
 
-def remove_columns(df):
-    """ Remove unnecessary columns.
-
-    :param df: DF of raw data
-    :return: DF
-    """
-    remove_cols = ["userID", "id", "replyToID", "replyToUser", "origPost",
-                   "Level", "Sampled", "CommentsAvail", "RepAvail", "FileNum",
-                   "verified", "quoted_text", "quoted_screen_name",
-                   "quoted_status_id", "quoted_favorite_count",
-                   "quoted_retweet_count", "quoted_verified", "retweet_text",
-                   "retweet_screen_name", "retweet_status_id",
-                   "retweet_favorite_count", "retweet_retweet_count",
-                   "retweet_verified", "source", "replyLevel"]
-
-    return df.drop(remove_cols, axis=1)
-
-
 def clean_text(text):
     """ Util: Cleans text string.
     > Lowercase string
@@ -41,37 +23,36 @@ def clean_text(text):
     > Replace numbers with "NUM" placeholder
 
     :param text: raw text string
+    :type text: str
     :return: cleaned text string
     """
     lowercased = text.lower()
     scores_removed = re.sub(r"(\d+) ?(-|:) ?(\d+)", "GAME_SCORE ", lowercased)
     punctuations = string.punctuation + "„" + "”"
-    punct_removed = scores_removed.translate(str.maketrans("", "", punctuations))
+    punct_removed = scores_removed.translate(str.maketrans("", "",
+                                                           punctuations))
     num_replaced = re.sub(r"\b\d+\b", "NUM ", punct_removed)
 
     return num_replaced
 
 
-def preprocess_data(df):
-    """ Preprocess data.
+def clean_dataframe(df):
+    """ Cleans dataframe:
     > Removes rows w/o text
-    > Removes columns, which are unnecessary for further modeling
     > Cleans text string
     > Tokenize text string
 
     :param df: raw DF
+    :type df: pd.DataFrame
     :return: cleaned DF
     """
     # Remove rows w/o text
-    data = df.dropna(subset=["text"], how="all")
-
-    # Remove unnecessary cols
-    cleaned_data = remove_columns(data)
+    data = df.dropna(subset=["text"], how="all").copy(deep=True)
 
     # Clean text
-    cleaned_data["cleaned_text"] = cleaned_data.text.apply(clean_text)
+    data["cleaned_text"] = data.text.apply(clean_text)
 
     # Tokenize text
-    cleaned_data["tokens"] = cleaned_data.cleaned_text.apply(word_tokenize)
+    data["tokens"] = data.cleaned_text.apply(word_tokenize)
 
-    return cleaned_data
+    return data
