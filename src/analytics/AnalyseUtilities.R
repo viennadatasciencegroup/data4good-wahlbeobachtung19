4b7a05f01d2a816f5964134ee41fdd63b731a855
Rania <33154189+rrania4r@users.noreply.github.com>
2019-10-02 15:24:38 +0200

    Unifying FB and Twitter files for further analysis

---
+++ src/analytics/AnalyseUtilities.R
@@ -0,0 +1,22 @@
+
+
+source("./R Code/hashSecrets.R")
+source("../Collect Comments/R Code/GeneralUtilityFuncs.R")
+# source("./R Code/GeneralCountingFuncs.R")
+source("../Collect Comments/R Code/TwitterCode/TwitterUtilityFuncs.R")
+source("../Collect Comments/R Code/FBCode/FaceBookUtilityFuncs.R")
+source("../Collect Comments/R Code/TwitterCode/TwCommentCollectionFuncs.R")
+# source("./R Code/TwitterCode/TwCommentSamplingFuncs.R")
+
+# source("./R Code/FBCode/myRfacebook/myGetCommentReplies.R")
+# source("./R Code/FBCode/myRfacebook/myGetPost.R")
+# source("./R Code/FBCode/myRfacebook/myGetPage.R")
+# source("./R Code/FBCode/myRfacebook/myRFacebook_utils.R")
+# source("./R Code/hashSecrets.R")
+# source("./R Code/GeneralUtilityFuncs.R")
+# source("./R Code/FBCode/FBCommentSamplingFunctions.R")
+
+source("./R Code/AnalyseUtilityFuncs.R")
+
+
+morePols <- c()
