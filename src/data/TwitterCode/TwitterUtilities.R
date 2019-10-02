1885826d5fa9c315f2a7371d1cc824b9539f82f7
Rania <33154189+rrania4r@users.noreply.github.com>
2019-10-02 15:20:47 +0200

    Code for downloading data from Twitter

---
+++ src/data/TwitterCode/TwitterUtilities.R
@@ -0,0 +1,10 @@
+
+
+source("./R Code/hashSecrets.R")
+source("./R Code/GeneralUtilityFuncs.R")
+source("./R Code/GeneralCountingFuncs.R")
+source("R Code/TwitterCode/TwitterUtilityFuncs.R")
+source("./R Code/TwitterCode/TwCommentCollectionFuncs.R")
+source("./R Code/TwitterCode/TwCommentSamplingFuncs.R")
+
+morePols <- c()
