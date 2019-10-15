d7615a1c675fa766aa87be68058021859e1a047f
datadonK23 <datadonk23@gmail.com>
2019-10-15 17:48:42 +0200

    Sentiment prototyping using spacy. Prototype stopped.

---
+++ notebooks/spacy_tokenize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+""" spacy_tokenize
+
+Util: Spacy tokenizer to import in NB (to avoid PickelingError in sklearn).
+
+Author: datadonk23
+Date: 15.10.19 
+"""
+
+import spacy
+from spacymoji import Emoji
+
+nlp = spacy.load("de_core_news_md", disable=["ner", "parser", "tagger"])
+emoji = Emoji(nlp, merge_spans=False)
+nlp.add_pipe(emoji, first=True)
+
+def spacy_tokenizer(doc):
+    tokens = [token for token in nlp(doc)]
+    cleaned_tokens = []
+    for token in tokens:
+        if not token.is_punct:
+            if not token.is_digit:
+                if not token.is_space:
+                    if not token.like_url:
+                        if not token.like_num:
+                            if not token.like_email:
+                                if not token.is_stop:
+                                    if len(token) > 3 or token._.is_emoji:
+                                        cleaned_tokens.append(token)
+    
+    return [token.orth_ for token in cleaned_tokens]
