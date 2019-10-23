801320008d13c95f1293b2b29cfe48d92f3867ec
datadonK23 <datadonk23@gmail.com>
2019-10-23 15:57:31 +0200

    Preprocessing script data access refator.

--- src/nlp/datasets_io.py
+++ src/nlp/datasets_io.py
@@ -7,7 +7,11 @@ Author: datadonk23
 Date: 29.09.19
 """
 
-import os
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(),
+                                             os.pardir, os.pardir, "config")))
+from config import credentials
+import dropbox
 import pandas as pd
 
 
@@ -20,10 +24,18 @@ def load_data(path):
     :param path: str
     :return: DF of raw data
     """
+    # Dropbox client
+    team_dbx = dropbox.DropboxTeam(credentials.dropbox_team_access_token)
+    team_root = team_dbx.with_path_root(dropbox.common.PathRoot.namespace_id(
+        credentials.dropbox_team_namespace_id))
+    user_dbx = team_root.as_user(credentials.dropbox_team_member_id)
+
     # Load Posts and Comments DF
-    posts = pd.read_csv(os.path.join(path, "PolPosts.csv"), low_memory=False)
-    comments = pd.read_csv(os.path.join(path, "UserComments.csv"),
-                           low_memory=False)
+    _, res_posts = user_dbx.files_download(os.path.join(path, "PolPosts.csv"))
+    posts = pd.read_csv(res_posts.raw, low_memory=False)
+    _, res_comments = user_dbx.files_download(os.path.join(path,
+                                                           "UserComments.csv"))
+    comments = pd.read_csv(res_comments, low_memory=False)
 
     # Merge Posts with Comments DFs
     raw_data = pd.concat([posts, comments], sort=False)
@@ -40,5 +52,13 @@ def dump_data(df, path):
     :param path: str
     :return: -
     """
+    # Dropbox client
+    team_dbx = dropbox.DropboxTeam(credentials.dropbox_team_access_token)
+    team_root = team_dbx.with_path_root(dropbox.common.PathRoot.namespace_id(
+        credentials.dropbox_team_namespace_id))
+    user_dbx = team_root.as_user(credentials.dropbox_team_member_id)
+
+    # Dump
     dump_path = os.path.join(path, "CleanedTextDF_interim.csv")
-    df.to_csv(dump_path, index=False)
+    user_dbx.files_upload(bytes(df.to_csv(index=False), "utf-8"),
+                          dump_path, mode=dropbox.files.WriteMode.overwrite)
